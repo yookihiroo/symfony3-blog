79fcc4c8a1d66a9623e1fb83556fff56501ec980
yoorwm <yookihiroo@gmail.com>
2015-12-19 15:32:46 +0000

    new action added

--- src/My/BlogBundle/Controller/DefaultController.php
+++ src/My/BlogBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace My\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use My\BlogBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
@@ -11,6 +13,35 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('MyBlogBundle:Post')->findAll();
         return $this->render('MyBlogBundle:Default:index.html.twig', ['posts' => $posts]);
+    }
+
+    public function newAction(Request $request)
+    {
+        $form = $this->createFormBuilder(new Post())
+            ->add('title')
+            ->add('body')
+            ->getForm();
+
+        if ('POST' == $request->getMethod()) {
+            $params = $request->request->all()['form'];
+            $form->submit($params);
+            if ($form->isValid()) {
+                // エンティティを永続化
+                $post = $form->getData();
+                $post->setCreatedAt(new \DateTime());
+                $post->setUpdatedAt(new \DateTime());
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($post);
+                $em->flush();
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // 描画
+        return $this->render('MyBlogBundle:Default:new.html.twig', array(
+                'form' => $form->createView(),
+        ));
     }
 
     public function showAction($id)
