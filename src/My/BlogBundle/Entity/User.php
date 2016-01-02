975f0f93558a0450c8a9a3acec8fba9dc6b7858a
yoorwm <yookihiroo@gmail.com>
2016-01-02 06:38:17 +0000

    test fos userbundle

---
+++ src/My/BlogBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace My\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="My\BlogBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
